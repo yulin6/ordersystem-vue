e7e24bc5af72d0aa028a4930bb6b385881d99fe7
lin.yu1@northeastern.edu <lin.yu1@husky.neu.edu>
2022-09-02 21:01:47 +0800

    implemented the sign-in interface

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,145 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+  <div class="login">
+    <div class="logo">
+      <h1>Foodies</h1>
+      <p v-show="isOwner" style="margin-left: 3px">Owner</p>
+      <p v-show="!isOwner" style="margin-left: 3px">Customer</p>
+    </div>
+
+
+    <el-card>
+      <el-form class="login-form"
+               :model="credentials"
+               :rules="rules"
+               ref="form"
+               @submit.prevent="login"
+      >
+        <el-form-item prop="username">
+          <el-input v-model="credentials.username" placeholder="Username" prefix-icon="User"> </el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="credentials.password" placeholder="Password" type="password" prefix-icon="Lock"> </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+              :loading="loading"
+              class="login-button"
+              type="primary"
+              native-type="submit"
+              block
+          >Get Started</el-button>
+        </el-form-item>
+      </el-form>
+      <el-button
+          v-show="!isOwner"
+          v-on:click="switchUserType"
+          type="primary"
+          link>
+        I'm a restaurant owner</el-button>
+      <el-button
+          v-show="isOwner"
+          v-on:click="switchUserType"
+          type="primary"
+          link>
+        I'm a customer</el-button>
+    </el-card>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data () {
+    return {
+      validCredentials: {
+        username: "testu",
+        password: "testp"
+      },
+      credentials: {
+        username: "",
+        password: ""
+      },
+      isOwner: false,
+      loading: false,
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "Username is required",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "Password is required", trigger: "blur" },
+          {
+            min: 5,
+            message: "Password length should be at least 5 characters",
+            trigger: "blur"
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    switchUserType() {
+      this.isOwner = !this.isOwner;
+      console.log(this.isOwner)
+    },
+    simulateLogin() {
+      return new Promise(resolve => {
+        setTimeout(resolve, 800);
+      });
+    },
+    async login() {
+      let valid = await this.$refs.form.validate();
+      if (!valid) {
+        return;
+      }
+      this.loading = true;
+      await this.simulateLogin();
+      this.loading = false;
+      if (
+          this.credentials.username === this.validCredentials.username &&
+          this.credentials.password === this.validCredentials.password
+      ) {
+        this.$message.success("Login successful");
+      } else {
+        this.$message.error("Username or password is invalid");
+      }
+    }
   }
 }
 </script>
+
+<style scoped>
+
+.login {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.el-card {
+  width: 400px;
+  display: flex;
+  justify-content: center;
+}
+.login-form {
+  width: 260px;
+}
+.login-button {
+  width: 100%;
+  margin-top: 20px;
+}
+</style>
 
 <style>
 #app {
@@ -22,5 +149,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo{
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: baseline;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
+// main.ts
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.use(ElementPlus)
+app.mount('#app')
