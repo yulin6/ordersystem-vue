45afdcb59eb371cbfd272612fe53be7efe4483a9
Mengting Chen <mengting.chen0222@gmail.com>
2022-09-27 04:11:47 +0800

    add AddCanteen dialog

---
+++ src/components/AddCanteen.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-dialog title="AddCanteen" @close='closeAddCanteen'>
+    <el-col>
+      <p>Restaurant Name:</p>
+      <el-input v-model="input" placeholder="text" clearable class="input"></el-input>
+      <p>Restaurant Type:</p>
+      <el-select v-model="value" clearable placeholder="Select one type" class="input">
+        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+        </el-option>
+      </el-select>
+    </el-col>
+    <el-button type="primary" class="button" v-on:click="confirmed">Confirm</el-button>
+    <!-- <el-button plain class="button" v-on:click="closeDialog">Cancel</el-button> -->
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'AddCanteen',
+  data() {
+    return {
+      propIsAddCanteenOpen: this.isAddCanteenOpen,
+      options: [{
+        value: 'option1',
+        label: 'Western'
+      }, {
+        value: 'option2',
+        label: 'Chinese'
+      }],
+      value: ''
+    }
+  },
+  methods: {
+    confirmed() {
+      this.$message({
+        message: 'A new restaurant has created successfully!',
+        type: 'success'
+      });
+    },
+    // closeDialog: function () {
+    //   this.isAddCanteenOpen = false
+    // }
+  }
+
+
+}
+</script>
+
+<style>
+.button {
+  margin-top: 16px;
+  width: 130px;
+}
+
+.input {
+  width: 400px;
+}
+</style>
--- src/components/AddCanteenCard.vue
+++ src/components/AddCanteenCard.vue
@@ -1,31 +1,33 @@
 <template>
 
-  <el-card style="cursor: pointer" shadow="hover" :body-style="{ padding: '0px' }" align="middle" v-on:click="enterCanteen" class="box-card">
+  <el-card style="cursor: pointer" shadow="hover" :body-style="{ padding: '0px' }" align="middle"
+    v-on:click="openAddCanteen" class="box-card">
     <div class="mid">
       <el-icon color="#3F9EFF" size="large" border-radius="20px">
         <Plus />
       </el-icon>
-      <p  style="margin-left: 0;color: #3F9EFF;font-weight: bold">Add a new restaurant!</p>
+      <p style="margin-left: 0;color: #3F9EFF;font-weight: bold">Add a new restaurant!</p>
+      <AddCanteen v-model="isAddCanteenOpen"></AddCanteen>
     </div>
   </el-card>
 
 </template>
 
 <script>
-import router from "@/router";
+import AddCanteen from "@/components/AddCanteen";
 
 export default {
   name: 'AddCanteenCard',
-  components: {},
-  methods: {
-    enterCanteen() {
-      console.log(this.name)
-      if (this.isOwner) {
-        router.push({ name: 'manage-canteen', params: { id: this.id } })
-      } else {
-        router.push({ name: 'canteen', params: { id: this.id } })
-      }
+  components: { AddCanteen },
+  data() {
+    return {
+      isAddCanteenOpen: false,
     }
+  },
+  methods: {
+    openAddCanteen() {
+      this.isAddCanteenOpen = true
+    },
   }
 
 }
