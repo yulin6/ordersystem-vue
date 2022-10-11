b2391fb396dcc3f0650d23822db04e34de59df5c
Mengting Chen <mengting.chen0222@gmail.com>
2022-10-11 23:03:26 +0800

    display dishes in ManageCanteen

--- src/views/ManageCanteen.vue
+++ src/views/ManageCanteen.vue
@@ -7,11 +7,12 @@
     <el-container>
       <el-aside style="width: 200px;">
         <el-menu default-active="1" class="el-menu-vertical-demo">
-          <el-menu-item v-for="(cat, index) in category" :index="cat" :key="index" v-on:click="jumpToCategory(cat)">
-            <input style="width: 100px" type="text" v-model="category[index]" v-show="isCatEditor" />
-            <span v-show="!isCatEditor">{{ cat }}</span>
-            <el-button style="margin-left: 6px" type="danger" @click="deleteRow(index, category)" v-show="isCatEditor"
-              size="small"> delete
+          <el-menu-item v-for="(dishType, index) in Object.keys(dishes)" :index="dishType" :key="index"
+            v-on:click="jumpToCategory(dishType)">
+            <input style="width: 100px" type="text" v-show="isCatEditor" />
+            <span v-show="!isCatEditor">{{ dishType }}</span>
+            <el-button style="margin-left: 6px" type="danger" @click="deleteRow(index, Object.keys(dishes))"
+              v-show="isCatEditor" size="small"> delete
             </el-button>
           </el-menu-item>
         </el-menu>
@@ -24,9 +25,9 @@
       </el-aside>
 
       <el-main>
-        <div v-for="cat in category" :key="cat" :id="cat" class="dishGroup">
-          {{ cat }}
-          <el-table :data="tableData" style="width: 100%; margin-top: 6px;">
+        <div v-for="(value, name, index) in dishes" :key="index" :id="name" class="dishGroup">
+          {{ name }}
+          <el-table :data="value" style="width: 100%; margin-top: 6px;">
             <el-table-column label="Dish Name" width="260">
               <template v-slot:default="scope">
                 <input type="text" v-model="scope.row.name" v-show="scope.row.isEditor" />
@@ -42,21 +43,21 @@
             </el-table-column>
             <el-table-column label="Quantity in Stock" width="200">
               <template v-slot:default="scope">
-                <el-input-number v-model="scope.row.quantity" :min="0" :max="10" v-show="scope.row.isEditor">
+                <el-input-number v-model="scope.row.stock" :min="0" :max="10" v-show="scope.row.isEditor">
                 </el-input-number>
-                <span v-show="!scope.row.isEditor">{{scope.row.quantity}}</span>
+                <span v-show="!scope.row.isEditor">{{scope.row.stock}}</span>
               </template>
             </el-table-column>
             <el-table-column label="Operation" width="260">
               <template v-slot:default="scope">
                 <el-button @click="edit(scope.row)">edit</el-button>
                 <el-button type="warning" @click="save(scope.row)">save</el-button>
-                <el-button type="danger" @click="deleteRow(scope.$index, tableData)"> delete
+                <el-button type="danger" @click="deleteRow(scope.$index, value)"> delete
                 </el-button>
               </template>
             </el-table-column>
           </el-table>
-          <el-button style="margin-top: 6px" type="primary" @click="addRow(tableData, false)">Add a new dish!
+          <el-button style="margin-top: 6px" type="primary" @click="addRow(value, false)">Add a new dish!
           </el-button>
         </div>
       </el-main>
@@ -67,43 +68,43 @@
 
 <script>
 import HomeMenu from "@/components/HomeMenu";
+import DishService from "@/services/DishService";
 import Utils from "@/utils/utils";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'ManageCanteen',
   components: { HomeMenu },
-  props: ['id'],
   data() {
     return {
-      category: ['Rice', 'Noodle', 'Dessert', 'Drink'],
+      loading: true,
+      id: this.$route.params.id,
+      canteenName: history.state.canteenName,
+      dishes: [],
+      dishService: DishService.getInstance(),
       isCatEditor: false,
-      tableData: [{
-        name: 'Chicken Sandwich',
-        price: 12,
-        quantity: 0,
-        isEditor: false
-      },
-      {
-        name: 'Cheese Burger',
-        price: 12,
-        quantity: 0,
-        isEditor: false
-      }]
     }
   },
   created() {
     Utils.storeUserFromLocal()
-    for (let i = 0; i < 10; ++i) {
-      this.tableData.push({
-        name: 'Chicken Sandwich',
-        price: 12,
-        quantity: 0,
-        isEditor: false
-      })
-    }
+    this.getDishes()
   },
   methods: {
+    async getDishes() {
+      await new Promise(r => setTimeout(r, 200)); //TODO DELETE, just for demonstration
+      await this.dishService.getDishes(this.id).then(res => {
+        if (res.code === 401) {
+          this.$message.error('Invalid login credential')
+          this.$router.push('/signin')
+          Utils.removeLocalData()
+        } else if (res.code === 200) {
+          this.dishes = res.data
+        } else {
+          this.$message.error(res.msg)
+        }
+      })
+      this.loading = false
+    },
     jumpToCategory(id) {
       document.getElementById(id).scrollIntoView();
     },
